54b2ede74bb8b14842a896773a18a3fb2f96234e
kodingkatmc <kodingkatmc@users.noreply.github.com>
2020-11-13 18:13:45 -0500

    added exp 2

--- ia.js
+++ ia.js
@@ -1,13 +1,9 @@
 console.log("Script Linked");
 
-function gen(table, letters, special, clickedFun){
-    
-}
-
 
 let table1 = document.getElementById('table1');
 let t1l = ['Z', 'N', 'V', 'K', 'M', 'X'];
-let numX = 0;
+let numX1 = 0;
 function clickedX(cell){
     
 }
@@ -16,13 +12,13 @@ document.getElementById('start1').addEventListener("click", function(){
     timer1 = new Date().getTime();
     console.log('starting exp1 at ' + timer1)
     table1.children[0].innerHTML = '';
-    numX = 0;
+    numX1 = 0;
     for (let i=0; i<8; i++){
         let row = table1.insertRow(0);
         for (let z=0; z<8; z++){
             let cell = row.insertCell(0);
-            //console.log((5-numX)/(49-(i*8 + z)));
-            if(Math.random()>((5-numX)/(64-(i*8 + z)))){
+            //console.log((5-numX1)/(49-(i*8 + z)));
+            if(Math.random()>((5-numX1)/(64-(i*8 + z)))){
                 cell.innerText = t1l[Math.floor(Math.random()*5)];
             }else{
                 cell.innerText = 'X';
@@ -31,16 +27,58 @@ document.getElementById('start1').addEventListener("click", function(){
                 cell.addEventListener("click", function(){
                     if(cell.innerText == 'X'){
                         console.log('x clicked');
-                        numX--;
+                        numX1--;
                         cell.innerText = '';
-                        if(numX==0){
+                        if(numX1==0){
                             document.getElementById('out1').textContent = 'Test Passed: ' + (new Date().getTime() - timer1);
                         }
                     }
                 });
-                numX ++;
+                numX1 ++;
             }
         }   
     }
-    //console.log(numX);
+    //console.log(numX1);
+});
+
+
+
+let table2 = document.getElementById('table2');
+let t2l = ['O', 'U', 'G', 'J', 'S', 'X'];
+let numX2 = 0;
+function clickedX(cell){
+    
+}
+
+document.getElementById('start2').addEventListener("click", function(){
+    timer2 = new Date().getTime();
+    console.log('starting exp2 at ' + timer2)
+    table2.children[0].innerHTML = '';
+    numX2 = 0;
+    for (let i=0; i<8; i++){
+        let row = table2.insertRow(0);
+        for (let z=0; z<8; z++){
+            let cell = row.insertCell(0);
+            //console.log((5-numX2)/(49-(i*8 + z)));
+            if(Math.random()>((5-numX2)/(64-(i*8 + z)))){
+                cell.innerText = t2l[Math.floor(Math.random()*5)];
+            }else{
+                cell.innerText = 'X';
+            }
+            if (cell.innerText == 'X'){
+                cell.addEventListener("click", function(){
+                    if(cell.innerText == 'X'){
+                        console.log('x clicked');
+                        numX2--;
+                        cell.innerText = '';
+                        if(numX2==0){
+                            document.getElementById('out2').textContent = 'Test Passed: ' + (new Date().getTime() - timer2);
+                        }
+                    }
+                });
+                numX2 ++;
+            }
+        }   
+    }
+    //console.log(numX2);
 });
